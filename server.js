82dbd46ea81fc76e9fcd67aa6cd517f3ab29b774
P3 <caleblgentry@gmail.com>
2019-09-03 10:02:36 -0400

    fixed...?

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic("/src")).listen(80, function(){
+connect().use(serveStatic("src")).listen(80, function(){
     console.log('Server running on 80...');
 });
